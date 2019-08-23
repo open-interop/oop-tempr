9f78d027a4d17a6f6c7872a7a9fb56a530e26575
Daniel Young <daniel.young@bluefrontier.co.uk>
2019-08-23 16:54:10 +0100

    fix: renaming env vars

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-    amqpAddress: process.env.AMQP_ADDRESS,
-    exchangeName: process.env.EXCHANGE_NAME,
-    endpointsExchangeName: process.env.ENDPOINTS_EXCHANGE_NAME,
-    translatedMessageQ: process.env.TRANSLATED_MESSAGE_Q,
+    amqpAddress: process.env.OOP_AMQP_ADDRESS,
+    exchangeName: process.env.OOP_EXCHANGE_NAME,
+    endpointsExchangeName: process.env.OOP_ENDPOINTS_EXCHANGE_NAME,
+    translatedMessageQ: process.env.OOP_HASAUTH_MESSAGE_Q,
     oopCoreApiUrl: process.env.OOP_CORE_API_URL,
-    oopEndpointsQ: process.env.OOP_ENDPOINTS_Q,
+    oopEndpointsQ: process.env.OOP_ENDPOINT_Q,
     oopNoEndpointQ: process.env.OOP_NO_ENDPOINT_Q,
     oopCoreToken: process.env.OOP_CORE_TOKEN
 };
