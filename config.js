a9117918fb16aefe7d0095b175bfed49d440770c
Daniel Young <daniel.young@bluefrontier.co.uk>
2019-09-20 10:37:54 +0100

    chore: upgrading package versions

--- config.js
+++ config.js
@@ -5,10 +5,8 @@ dotenv.config();
 module.exports = {
     amqpAddress: process.env.OOP_AMQP_ADDRESS,
     exchangeName: process.env.OOP_EXCHANGE_NAME,
-    endpointsExchangeName: process.env.OOP_ENDPOINTS_EXCHANGE_NAME,
-    translatedMessageQ: process.env.OOP_HASAUTH_MESSAGE_Q,
+    temprInputQ: process.env.OOP_TEMPR_INPUT_Q,
+    temprOutputQ: process.env.OOP_TEMPR_OUTPUT_Q,
     oopCoreApiUrl: process.env.OOP_CORE_API_URL,
-    oopEndpointsQ: process.env.OOP_ENDPOINT_Q,
-    oopNoEndpointQ: process.env.OOP_NO_ENDPOINT_Q,
     oopCoreToken: process.env.OOP_CORE_TOKEN
 };
